d233d5a09cdd526f55f6939f30829dea77130dba
ndmanh.dhkl16a2hn@sv.uneti.edu.vngit config --global user.name ndmanh.dhkl16a2hn@sv.uneti.edu.vngit config --global user.name ndmanh.dhkl16a2hn@sv.uneti.edu.vn <ndmanh.dhkl16a2hn@sv.uneti.edu.vn>
2022-12-07 13:57:19 +0700

    b

---
+++ mang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+int m,n,a[1005][1005];
+cin>>m;
+cin>>n;
+for(int i=1;i<=m;i++)
+{
+    for(int j=1j;<=n;j++)
+    cin<<a[i][j];
+}
+cout<<a[i][j];
